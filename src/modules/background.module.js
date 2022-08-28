5da59095a99e57e8bfea5e3d1e3c322bb1edce23
kirillStudent <kononovkirill05@gmail.com>
2022-08-28 19:26:14 +0600

    init feature/click-analytics

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,20 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
+    constructor(type, text) {
+        super(type, text)
+    }
 
+    #getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        };
+        return color;
+      };
+
+    trigger() {
+        document.body.style.backgroundColor = this.#getRandomColor();
+    };
 }
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,24 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
+    #scope
 
+    constructor(type, text) {
+        super(type, text);
+
+        this.#scope = document.querySelector( 'body' );
+    };
+
+    trigger() {
+        let clickCount = -1;
+        function handleMouseClick(event) {
+            clickCount++;
+          };
+        
+        this.#scope.addEventListener('click', handleMouseClick);
+        setTimeout(() => {
+            console.log(clickCount);
+            this.#scope.removeEventListener('click', handleMouseClick)
+        }, 5000);
+    };
 }
